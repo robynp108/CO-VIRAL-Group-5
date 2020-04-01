541839c45da0477969cbfd19649e28f06803acfc
Migs <mmejares@hotmail.com>
2020-03-31 22:44:16 -0500

    updated js

--- datalogic.js
+++ datalogic.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
 
 var queryURL = "https://corona-api.com/countries"
+var globalCases = 0;
+var globalDeaths = 0;
+var globalCritical = 0;
+var globalRecovered = 0;
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
     console.log(response);
-    console.log(response.data[213]);
+    console.log(response.data[213].latest_data.confirmed);
+   
+
+
     
 })
 
